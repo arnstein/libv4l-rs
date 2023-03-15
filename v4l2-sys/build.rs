1ad0e52495882df45f5e23fdf2f2fb6c0022eeb8
Arnstein Kleven <arnsteinkleven@gmail.com>
2023-03-15 12:00:46 +0100

    Update build.rs to work with Yocto bb

--- v4l2-sys/build.rs
+++ v4l2-sys/build.rs
@@ -4,8 +4,17 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+
+	println!("cargo:rustc-link-search=/usr/lib");
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let sysroot = format!("--sysroot={}", env::var("LIBCLANG_PATH").unwrap());
+    let incdir = format!("-I{}", env::var("VIDEODEV_PATH").unwrap());
+
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
+		.clang_arg(sysroot)
+		.clang_arg(incdir)
         .generate()
         .expect("Failed to generate bindings");
 
